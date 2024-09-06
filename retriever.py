66b95313ceb45c290821fa47aa50d32033b550d6
Jang-DongHo <donghoo18@naver.com>
2024-09-06 15:07:05 +0900

    잘 된거 저장

--- retriever.py
+++ retriever.py
@@ -1,11 +1,14 @@
 from langchain import hub
-from langchain_text_splitters import MarkdownHeaderTextSplitter
+from langchain_text_splitters import MarkdownHeaderTextSplitter, HTMLHeaderTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
+from langchain.docstore.document import Document
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_teddynote import logging
+from bs4 import BeautifulSoup
+from io import StringIO
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -30,21 +33,38 @@ markdown_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_sp
 # markdown_document를 헤더를 기준으로 분할하여 md_header_splits에 저장합니다.
 widget_doc = markdown_splitter.split_text(docs[0].page_content)
 
-loader = TextLoader("./dataset/test.xhtml", encoding="utf-8")
-epub_docs1 = loader.load()
+
 loader = TextLoader("./dataset/test2.xhtml", encoding="utf-8")
-epub_docs2 = loader.load()
-loader = TextLoader("./dataset/test3.xhtml", encoding="utf-8")
-epub_docs3 = loader.load()
+docs = loader.load()
+
+# Create a BeautifulSoup object with recursive=False
+soup = BeautifulSoup(docs[0].page_content, 'html.parser')
+body = soup.find('body')
+epub_doc = []
+
+if body:
+    for idx, element in enumerate(body.find_all(recursive=False)):
+        metadata = {
+            'element_type': element.name,
+            'attributes': element.attrs,
+            'id': element.get('id', ''),
+            'class': element.get('class', []),
+            'text_content': element.get_text(strip=True),
+            'parent_element': element.find_parent().name if element.find_parent() else None,
+            'position': idx,
+            'document_location': f'document_{idx}'
+        }
+        epub_doc.append(Document(metadata=metadata, page_content=str(element)))
+
 
 # 단계 3: 임베딩 & 벡터스토어 생성(Create Vectorstore)
 # 벡터스토어를 생성합니다.
 embedding_model = GoogleGenerativeAIEmbeddings(model='models/embedding-001')
-vectorstore = FAISS.from_documents(documents=widget_doc + epub_docs2, embedding=embedding_model)
+vectorstore = FAISS.from_documents(documents=widget_doc+epub_doc, embedding=embedding_model)
 
 # 단계 4: 검색(Search)
 # 기술 문서에 포함되어 있는 정보를 검색하고 생성합니다.
-retriever = vectorstore.as_retriever(search_type="mmr", search_kwargs={"k": 4, "fetch_k": 10})
+retriever = vectorstore.as_retriever(search_type="mmr", search_kwargs={"k": 6, "fetch_k": 40})
 
 # 단계 5: 프롬프트 생성(Create Prompt)
 # 프롬프트를 생성합니다.
@@ -56,6 +76,7 @@ prompt = hub.pull("rlm/rag-prompt")
 # 단계 7: 체인 생성(Create Chain)
 def format_docs(docs):
     # 검색한 문서 결과를 하나의 문단으로 합쳐줍니다.
+    print("\n\n".join(doc.page_content for doc in docs))
     return "\n\n".join(doc.page_content for doc in docs)
 
 # 단계 8: 체인 실행(Run Chain)
@@ -80,7 +101,6 @@ json_schema = """
                             "properties": {
                                 "example_code": {
                                     "type": "string"
-                                }
                             }
                         }
                     }
@@ -98,15 +118,11 @@ json_schema = """
 # 템플릿 질문 정의
 question_template = """
 Please bring the names, descriptions, and xhtml code of the widgets related to the following sentence.
-If you have multiple widgets, bring them all.
-
-Widget descriptions may or may not be present.
 
 sentence:
     {style_guide}
 
 The output should be formatted as a JSON instance that conforms to the JSON schema below.
-Include as much example_code as possible.
 
 
 Here is the output schema:
